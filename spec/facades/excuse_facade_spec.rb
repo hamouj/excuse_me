e754f6286779ce96735398c81f6b2b386004758a
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-06-05 12:31:10 -0700

    test: test fetch_excuse_by_topic method

--- spec/facades/excuse_facade_spec.rb
+++ spec/facades/excuse_facade_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 describe ExcuseFacade do
-  let(:facade) { ExcuseFacade.new }
+  let(:facade) { ExcuseFacade.new({excuse: "true", excuse_topic: "funny"}) }
 
   describe '#initialize' do
     it 'exists' do
       expect(facade).to be_an ExcuseFacade
+      expect(facade.excuse).to eq("true")
+      expect(facade.topic).to eq("funny")
     end
   end
 
@@ -15,4 +17,12 @@ describe ExcuseFacade do
       expect(facade.fetch_random_excuse.excuse).to be_a String
     end
   end
+
+  describe '#fetch_excuse_by_topic' do
+    it 'creates and Excuse object' do
+      expect(facade.fetch_excuse_by_topic).to be_an Excuse
+      expect(facade.fetch_random_excuse.excuse).to be_a String
+    end
+  end
+  
 end
