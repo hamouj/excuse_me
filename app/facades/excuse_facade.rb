c1aa6f67a3a10be50e5b8050d5ad4e9caf6fd2d8
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-06-05 12:31:24 -0700

    feat: create fetch_excuse_by_topic method

--- app/facades/excuse_facade.rb
+++ app/facades/excuse_facade.rb
@@ -1,6 +1,18 @@
 class ExcuseFacade
+  attr_reader :excuse, :topic
+
+  def initialize(params)
+    @excuse = params[:excuse]
+    @topic = params[:excuse_topic]
+  end
+
   def fetch_random_excuse
     data = ExcuserService.get_random_excuse
+    Excuse.new(data[0])
+  end
+
+  def fetch_excuse_by_topic
+    data = ExcuserService.get_topic_excuse(@topic.downcase)
     Excuse.new(data[0])
   end
 end
